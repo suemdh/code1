64757cd2c4fad27965def5ecc8605adc7dcc3ec0
suemdh <85892088+suemdh@users.noreply.github.com>
2021-07-19 20:49:16 -0400

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,31 @@
+class ground
+{
+constructor(x,y,w,h)
+{
+let options = {
+isStatic:true
+};
+
+
+this.x=x;
+this.y=y;
+this.w = w
+this.h = h;
+this.body= Bodies.rectangle(x,y,w,h,options);
+World.add(world,this.body);
+}
+
+
+show()  {
+var pos = this.body.position;
+push();
+rectMode(CENTER);
+stroke(255);
+fill(127);
+rect(pos.x,pos.y,this.w,this.h)
+pop();
+}
+
+
+
+}
